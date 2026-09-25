20a25c079cd3ccb2f4357c1d81e740f02510e2ee
Junior-passy2025 <juniorpassy12@gmail.com>
2026-09-25 08:50:28 +0100

    test hero about only

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+// import Skills from "./components/Skills";
+// import Experience from "./components/Experience";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 
 function App() {
 return (
@@ -22,10 +22,7 @@ return (
   <main>
     <Hero />
     <About />
-    <Skills />
-    <Experience />
-    <Projects />
-    <Contact />
+  
   </main>
 
 </div>
