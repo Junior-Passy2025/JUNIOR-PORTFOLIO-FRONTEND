234cb5c8943b58b9e80b6cc8684a21f1ac230e08
Junior-passy2025 <juniorpassy12@gmail.com>
2026-09-25 09:39:57 +0100

    restore all portfolio sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -15,7 +20,12 @@ function App() {
       <Navbar />
 
       <main>
+        <Hero />
         <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Contact />
       </main>
 
     </div>
